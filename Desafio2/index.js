f7eec13c9a59973537cd382e93405cd3fb0286d0
magavercelli <magabrielavercelli@hotmail.com>
2023-11-16 16:32:51 -0300

    segundo desafio

---
+++ Desafio2/index.js
@@ -0,0 +1,56 @@
+import ProductManager from "./managers/ProductManager.js";
+
+const manager = new ProductManager('./files/productos.json');
+
+const enviar = async () => {
+    try {
+        const products = await manager.getProduct();
+        console.log(products);
+
+        const nuevoProducto = {
+            title: 'Producto 1',
+            description: 'Descripcion1',
+            price: 500,
+            thumbnail: 'sin imagen',
+            code: 'abc123',
+            stock: 50
+        };
+        
+        const addedProduct = await manager.addProduct(
+            nuevoProducto.title,
+            nuevoProducto.description,
+            nuevoProducto.price,
+            nuevoProducto.thumbnail,
+            nuevoProducto.code,
+            nuevoProducto.stock
+        );
+        console.log('Nuevo producto añadido:', addedProduct);
+
+        
+        const productId = 1; 
+        const productById = await manager.getProductById(productId);
+        console.log('Producto por ID:', productById);
+
+        const updatedProductId = 1; 
+        const updatedProduct = {
+            title: 'Producto 2',
+            description: 'descripcion2',
+            price: 700,
+            thumbnail: 'imagen2',
+            code: 'def456',
+            stock: 30
+        };
+        const updatedProductResult = await manager.updateProduct(updatedProductId, updatedProduct);
+        console.log('Producto actualizado:', updatedProductResult);
+
+ 
+        const deletedProductId = 2; 
+        const deletedProduct = await manager.deleteProduct(deletedProductId);
+        console.log('Producto eliminado:', deletedProduct);
+    } catch (error) {
+        console.error(error);
+    }
+    
+}; 
+
+enviar()
---
+++ Desafio2/managers/ProductManager.js
@@ -0,0 +1,105 @@
+import fs from 'fs';
+
+export default class ProductManager {
+    constructor(path){
+        this.products = [];
+        this.path = './files/productos.txt'
+    }
+
+    getProduct = async () => {
+
+        try {
+            if(fs.existsSync(path)){
+                const data =await fs.promises.readFile(this.path, 'utf-8');
+                const product =JSON.parse(data)
+                return product
+            }else {
+                return [];
+            }
+            
+        } catch (error) {
+            console.log(error)
+            
+        }
+    }
+
+    addProduct = async (title, description, price, thumbnail, code, stock) => {
+        if (!title || !description || !price || !thumbnail || !code || !stock) {
+            console.log ('Todos los campos son obligatorios')
+                return
+            }
+        
+        const product = await this.getProduct();
+        if (this.product.some(product => product.code === code)){
+            console.log('El codigo ya existe');
+                return
+
+        }
+
+        if (product.length === 0){
+            product.id = 1
+        }else{
+            product.id = product[product.length-1].id+1;
+        }
+
+        const newProduct = {
+            title,
+            description,
+            price,
+            thumbnail,
+            code,
+            stock,
+        };
+
+        product.push(newProduct);
+        await fs.promises.writeFile(path, JSON.stringify(product, null, '\t'))
+        return product;
+        
+    }
+
+    getProductById = async (idProduct) => {
+        const products = await this.getProduct();
+        const product = this.products.find(product => product.id === idProduct);
+        if (product) {
+            return product;
+        } else {
+            return 'Not found';
+        }
+
+    }
+
+    updateProduct = async (id, updatedProduct) => {
+        const products = await this.getProduct();
+        const index = products.findIndex(product => product.id === id);
+
+        if (index !== -1) {
+            
+            products[index] = { ...products[index], ...updatedProduct };
+
+            
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+
+            return products[index];
+        } else {
+            return 'Product not found';
+        }
+    }
+
+    deleteProduct = async (idProduct) => {
+        const products = await this.getProduct();
+        const index = products.findIndex(product => product.id === idProduct);
+    
+        if (index !== -1) {
+            
+            const deletedProduct = products.splice(index, 1)[0];
+    
+         
+            await fs.promises.writeFile(this.path, JSON.stringify(products, null, '\t'));
+    
+            return deletedProduct;
+        } else {
+            return 'Product not found';
+        }
+    }
+}
+
